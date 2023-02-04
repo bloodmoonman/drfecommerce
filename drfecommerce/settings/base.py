8f6495a10668e86f94da8e0c4d18a882e1b7852a
Aykan Seper <aykanseper@gmail.com>
2023-02-04 23:23:34 +0300

    setting environment variables

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j51s#8!%zd%mz=m)#dy(3x3tqd4b2r$!rv)51)#^=77xfdoca&'
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
