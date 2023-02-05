b9300198c00270084b7066007dd29d41d6fb3109
Aykan Seper <96862833+bloodmoonman@users.noreply.github.com>
2023-02-05 14:27:05 +0300

    initialized test

---
+++ drfecommerce/tests/test_example.py
@@ -0,0 +1,5 @@
+
+def test_example():
+    assert 1 == 1 #assert returns True or False
+                  #left value is our setup the right one is what
+                  #we are going to test against 
